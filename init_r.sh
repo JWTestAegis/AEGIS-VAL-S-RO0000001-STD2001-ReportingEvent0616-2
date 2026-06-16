fb51360f3ee84e7e9ef57cd26e1ea373e0a72367
JiangWei2025 <daniel.jiang.w@foxmail.com>
2026-06-16 13:41:32 +0800

    Update init_r.sh with testing comment
    Added a comment indicating the script is updated for testing.

--- init_r.sh
+++ init_r.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # **************************************************************
 # Copy .Rprofile and .Renviron to project directory
+# updated for testing
 # **************************************************************
 echo "Current Directory: $(pwd)"
 echo "*** File list under project directory BEFORE copying ***"
